6d272a9bb05aec675e33cbb046d4c04e88466b2a
mishka <mishka@nest-32s-iMac.local>
2023-04-05 19:39:10 +0800

    3.26

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Intro from "./company/intro"
+import Map from "./company/map"
+
+export default function App() {
+  return (
+    <React.Fragment>
+    <Intro/>
+    <Map/>
+    </React.Fragment>
+  )
+}
