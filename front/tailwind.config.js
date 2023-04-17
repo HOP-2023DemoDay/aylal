c16afc1f2e51464e91fe7149dcbcee39a2418b07
mishka <mishka@nest-32s-iMac.local>
2023-04-17 19:20:33 +0800

    hutulbur

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -14,7 +14,14 @@ module.exports = {
         Button: "#43B97F",
         Black: "#141414",
       },
-      
+
+      screens: {
+        sm: {min: '640px', max: '767px'},
+        md: {min: '768px', max: '1023px'},
+        lg: {min: '1024px', max: '1279px'},
+        xl: {min: '1280px', max: '1535px'},
+        Txl: {min: '1536px'},
+      },
     },
   },
   plugins: [],
