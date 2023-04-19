3f232dd0a64c2c81def7f866765760032cdb3d5a
mishka <mishka@nest-32s-iMac.local>
2023-04-19 19:45:29 +0800

    4-19

---
+++ front/src/components/news.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import img from "../img/img3.png"
+
+export const news = () => {
+  return (
+    <div>
+        <img src={img} alt="" />
+    </div>
+  )
+}
+export default news
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       },
 
       screens: {
+        smm:{min: "0px" , max:"639px"},
         sm: {min: '640px', max: '767px'},
         md: {min: '768px', max: '1023px'},
         lg: {min: '1024px', max: '1279px'},
