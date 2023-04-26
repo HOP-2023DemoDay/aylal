d87e0e67964ac83f29107e54ff368d8d1cce8f29
Natsagdorj <natsagdorj@niggers.local>
2023-04-26 17:25:04 +0800

    backend ehlel

--- back/helper/db.js
+++ back/helper/db.js
@@ -2,37 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const MongoClient = require("mongodb").MongoClient;
 const uri = process.env.mongodb;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// client.connect((err) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-
-//   const collection = client.db("test").collection("images");
-//   const image = {
-//     name: req.file.originalname,
-//     data: req.file.buffer,
-//     contentType: req.file.mimetype,
-//   };
-
-//   collection.insertOne(image, (err, result) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-
-//     console.log(result);
-//     res.send("File uploaded!");
-//   });
-// });
-
 const connect = async () => {
   try {
     await mongoose.connect(uri);
