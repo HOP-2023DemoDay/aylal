76f1dd68e130d8ff79a8cc0eeb11ca4b7badeddb
MunkhlunFlew4k <munkhlunflew4k@Nest-28.local>
2023-04-28 17:56:23 +0800

    updated

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       screens: {
         sm: { min: "640px", max: "767px" },
-        md: { min: "768px", max: "1023px" },
+        md: { min: "768px", max: "1024px" },
         lg: { min: "1024px", max: "1279px" },
         xl: { min: "1280px", max: "1535px" },
         "2xl": { min: "1536px" },
