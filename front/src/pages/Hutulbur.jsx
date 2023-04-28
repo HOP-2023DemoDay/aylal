3ae754dd2becdb457a8f628b6ffa62805b247242
MunkhlunFlew4k <munkhlunflew4k@Nest-28.local>
2023-04-28 17:59:53 +0800

    reset

--- front/src/pages/Hutulbur.jsx
+++ front/src/pages/Hutulbur.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Arrow from "../img/Vector.png";
-import Map from "../img/map.png";
-import Lake from "../img/lake.png";
-import News from "../components/news";
-import clsx from "clsx";
-import Mountain from "../img/mountain.png";
-import Link from "react-router-dom";
+// import React from "react";
+// import Arrow from "../img/Vector.png";
+// import Map from "../img/map.png";
+// import Lake from "../img/lake.png";
+// import News from "../components/news";
+// import clsx from "clsx";
+// import Mountain from "../img/mountain.png";
+// import Link from "react-router-dom";
 
-export const Hutulbur = () => {
-  return (
-    <div>
-      <img src={Mountain} className="w-[100vw] h-[30vh]">
-        <Link></Link>
-      </img>
-    </div>
-  );
-};
-export default Hutulbur;
+// export const Hutulbur = () => {
+//   return (
+//     <div>
+//       <img src={Mountain} className="w-[100vw] h-[30vh]">
+//         <Link></Link>
+//       </img>
+//     </div>
+//   );
+// };
+// export default Hutulbur;
