f897047483dee09a5142a2febd0921fac107eb00
oyuka <oyuka@LEAP-16s-iMac.local>
2023-05-03 19:37:44 +0800

    5.3 figma uurchluh,readmedeer ajlih bolson udur//guitsee

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import Hutulbur from "./pages/Hutulbur";
 import CompanyInfo from "./pages/CompanyInfo";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/CompanyInfo" element={<CompanyInfo />}></Route>
         <Route path="/Hutulbur" element={<Hutulbur />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
