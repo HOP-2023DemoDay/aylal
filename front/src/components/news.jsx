cb18e33a7a3fb6e3335defe0983f0ac19610739e
oyuka <oyuka@LEAP-16s-iMac.local>
2023-05-10 16:50:04 +0800

    rebase hiisen

--- front/src/components/news.jsx
+++ front/src/components/news.jsx
@@ -4,7 +4,7 @@ import img from "../img/img3.png"
 export const news = () => {
   return (
     <div className='mt-[6vh]'>
-        <img src={img} alt="" className='md:h-[210px] md:w-[300px] smmsm: w-[340px]' />
+        <img src={img} alt="" className='md:h-[210px] md:w-[300px]' />
         <div className='mt-[-20vh] ml-[15px]'>
         <div className='font-bold text-[19.2px]  text-white w-[320px] md:text-[17px] md:w-[250px]'>Wakatobi beach is a paradise for coral reets in indonesia</div>
         <div className='text-[12px] font-bold  text-white '>Yogyakarta, Indonesia</div>
