ff202032ad29e3a691df0379ed165ef9190a1601
MunkhlunFlew4k <munkhlunflew4k@Nest-28.local>
2023-05-15 19:35:19 +0800

    posthutulburfront

--- front/src/pages/Post.jsx
+++ front/src/pages/Post.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Arrow from "../img/Vector.png";
+import axios from "axios";
 
 export const Post = () => {
+  const [selectedImage, setSelectedImage] = useState(false);
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+  const handleFileUpload = async (e) => {
+    const file = e;
+    const base64 = await convertToBase64(file);
+    console.log(base64);
+    // axios.post("http://www.localhost:8000/hutulburs", {
+    //   image: base64,
+    // });
+  };
   return (
     <div>
       <div className="bg-cover w-[100vw] h-[35vh] p-[5vw] flex flex-col gap-[2.5vw]">
@@ -20,14 +42,40 @@ export const Post = () => {
           </p>
           <input className="text-[16px] w-[40vw] h-[35vh] border border-black rounded-xl pb-[28vh] pl-[27px] f-Poppins" />
 
-          <img alt="" className="rounded-2xl w-[38vw]" />
+          {selectedImage ? (
+            <div>
+              <img
+                alt="not found"
+                width={"250px"}
+                className="w-[40vw] h-[20vh]"
+                src={URL.createObjectURL(selectedImage)}
+              />
+              {/* <button onClick={() => setSelectedImage(null)}>Remove</button> */}
+            </div>
+          ) : (
+            
+            <input
+              type="file"
+              name="img"
+            //   name="myImage"
+              className="w-[40vw] h-[20vh]"
+              onChange={(event) => {
+                console.log(event.target.files[0]);
+                setSelectedImage(event.target.files[0]);
+              }}
+            />
+          )}
+
           <div className="flex flex-col gap-3">
             <p className="font-normal text-[1.2vw] font-Poppins text-DarkText">
               Comment :
             </p>
             <input className="w-[40vw] h-[4vh] border border-black rounded-full focus:border-sky-500" />
-            <button class="bg-Button hover:bg-ButtonHover font-[400] font-Poppins w-[5vw] h-[4vh] rounded-lg text-white">
-              Send
+            <button
+              class="bg-Button hover:bg-ButtonHover font-[400] font-Poppins w-[5vw] h-[4vh] rounded-lg text-white"
+              onClick={() => handleFileUpload(selectedImage)}
+            >
+              Post
             </button>
           </div>
         </div>
