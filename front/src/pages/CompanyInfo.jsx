b909066e909ad388a71326cd9ae0d2fc823d9f5f
MunkhlunFlew4k <munkhlunflew4k@Nest-28.local>
2023-05-15 18:37:31 +0800

    Postfront

--- front/src/pages/CompanyInfo.jsx
+++ front/src/pages/CompanyInfo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import clsx from "clsx";
 import Hutulbur from "../components/Hutulbur";
-import Map from "../img/map.png"
-import star from "../img/star.svg"
-import hun from "../img/hun.png"
-import img from "../img/companyImg.svg"
-import pagearr from "../img/pagearr.svg"
+import Map from "../img/map.png";
+import star from "../img/star.svg";
+import hun from "../img/hun.png";
+import img from "../img/companyImg.svg";
+import pagearr from "../img/pagearr.svg";
 
 const style = {
-  people: "2xl:w-[40px] 2xl:h-[40px] 2xl:ml-[-2vw] xl:w-[40px] xl:h-[40px] xl:ml-[-2vw] lg:w-[40px] lg:h-[40px] lg:ml-[-3vw] md:w-[30px] md:h-[30px] md:ml-[-3vw] w-[26px] h-[26px] ml-[-6vw] bg-DarkText rounded-full",
+  people:
+    "2xl:w-[40px] 2xl:h-[40px] 2xl:ml-[-2vw] xl:w-[40px] xl:h-[40px] xl:ml-[-2vw] lg:w-[40px] lg:h-[40px] lg:ml-[-3vw] md:w-[30px] md:h-[30px] md:ml-[-3vw] w-[26px] h-[26px] ml-[-6vw] bg-DarkText rounded-full",
   box: "flex justify-between w-[281px] mt-[2vh] ml-[20vw] md:ml-[10vw] xl:ml-[25vw] 2xl:ml-[0vw]",
   too: "2xl:ml-[5vw] 2xl:text-[35px] xl:ml-[-0.5vw] lg:ml-[-1vw] md:ml-[0.5vw] font-Poppins font-light text-[22px] ml-[-22vw] opacity-50 text-white",
   star: "2xl:w-[35px] 2xl:h-[35px] 2xl:ml-[7vw] xl:ml-[2vw] w-[22px] lg:ml-[3vw] h-[22px] ml-[-12vw] opacity-50 ml-[-10vw] md:w-[20px] md:h-[20px] md:ml-[5vw] mt-[-3vh]",
@@ -19,90 +20,117 @@ export const CompanyInfo = () => {
   return (
     <div className="overflow-hidden w-screen">
       <div>
-      <img src={img} alt="img" className="w-screen h-[20vh] object-cover " />
-      <button><img src={pagearr} alt="arrow" onClick = {() => {console.log("first")}} className="h-10 mt-[-30vh] ml-[2vw]" /></button>
-      <div className="w-screen flex justify-around mt-[-9vh]">
-        <p className="font-Poppins font-bold text-[22px] text-white 2xl:text-[50px] ml-[-16vw] md:text-[28px] md:text-[24px]">CompanyName</p>
-        <div className="w-[15vw] flex justify-between mr-[-20vw]">
-          <div className="w-[90px] ml-[-5vw]">
-          <p className={clsx(style.too)}>144</p>
-          <img src={star} alt="start" className={clsx(style.star)}/>
-          </div>
-          <div className="w-[140px] flex ml-[-5vw] lg:ml-[10vw] md:ml-[10vw]">
-            <img src={hun} alt="hun" className={clsx(style.people)}/>
-            <img src={hun} alt="hun" className={clsx(style.people)}/>
-            <img src={hun} alt="hun" className={clsx(style.people)}/>
-            <img src={hun} alt="hun" className={clsx(style.people)}/>
-            <img src={hun} alt="hun" className={clsx(style.people)}/>
+        <img src={img} alt="img" className="w-screen h-[20vh] object-cover " />
+        <button>
+          <img
+            src={pagearr}
+            alt="arrow"
+            onClick={() => {
+              console.log("first");
+            }}
+            className="h-10 mt-[-30vh] ml-[2vw]"
+          />
+        </button>
+        <div className="w-screen flex justify-around mt-[-9vh]">
+          <p className="font-Poppins font-bold text-[22px] text-white 2xl:text-[50px] ml-[-16vw] md:text-[28px] md:text-[24px]">
+            CompanyName
+          </p>
+          <div className="w-[15vw] flex justify-between mr-[-20vw]">
+            <div className="w-[90px] ml-[-5vw]">
+              <p className={clsx(style.too)}>144</p>
+              <img src={star} alt="start" className={clsx(style.star)} />
+            </div>
+            <div className="w-[140px] flex ml-[-5vw] lg:ml-[10vw] md:ml-[10vw]">
+              <img src={hun} alt="hun" className={clsx(style.people)} />
+              <img src={hun} alt="hun" className={clsx(style.people)} />
+              <img src={hun} alt="hun" className={clsx(style.people)} />
+              <img src={hun} alt="hun" className={clsx(style.people)} />
+              <img src={hun} alt="hun" className={clsx(style.people)} />
+            </div>
           </div>
         </div>
       </div>
-      </div>
       <div className="2xl:mt-[20vh] 2xl:px-[5vw] xl:mt-[10vh] lg:mt-[10vh] lg:px-[5vw] md:mt-[5vh] md:px-[5vw] flex justify-around md:mt-[10vh] mt-[10vh] ml-[-3vw]">
-
         <div className="2xl:w-1/2 2xl:mr-[5vw] 2xl:ml-[-5vw] xl:ml-[-10vw] md:ml-[5vw] ml-[5vw] lg:ml-[-18vw]">
-        <p className="text-[26px] ml-[5vw] font-Poppins text-DarkText font-bold lg:text-[26px] md:ml-[-5vw] md:text-[24px] 2xl:text-[40px] 2xl:ml-[10vw] md:ml-[0vw]">Get in touch</p>
-        <p className={clsx(style.Text)} >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Exercitationem facere voluptatum ab, cum fugiat asperiores amet sit
-          tenetur maiores unde! In omnis corporis, natus veniam aliquid nesciunt
-          quod pariatur inventore harum ratione velit facere nisi possimus,
-          repellat asperiores aliquam, officiis porro iste tempora doloremque
-          veritatis doloribus atque expedita! Maxime sunt obcaecati debitis
-          expedita. Aut debitis aliquam nostrum libero. Tempore quam ipsum
-          provident sequi saepe nobis repellat dolorem cumque similique
-          quibusdam quo, nam aliquam, quidem voluptate dignissimos vitae dolor?
-          Animi aspernatur, necessitatibus magni cumque minus quisquam porro
-          dolor sunt. Harum facilis sit quis, soluta ullam excepturi. Unde eum
-          debitis blanditiis molestias aperiam cupiditate labore, soluta
-          corporis repellat ab dolorem. Veritatis ad ducimus minima quis iure,
-          dolores optio aut assumenda fugiat enim velit quaerat corrupti odit
-          numquam commodi? Sequi velit facere aliquam aliquid. Consectetur
-          similique error nostrum fugit tenetur? Laborum incidunt saepe amet
-          optio consequuntur ipsa similique illum neque! Quas magni id, dolorem
-          possimus facilis autem fuga expedita natus qui porro architecto
-          tempora beatae laboriosam fugiat culpa animi distinctio totam vitae
-          sint dicta incidunt aut! Iusto ipsa ut recusandae ducimus veniam
-          deserunt omnis perferendis totam impedit natus repudiandae voluptas
-          illo aliquid repellendus, adipisci deleniti reiciendis nobis a vero?
-          Cupiditate aspernatur vero hic eaque nihil id vitae dicta, molestias
-          odit ducimus illum, fuga libero? Quae pariatur cumque rerum quas
-          doloremque id impedit modi minima deleniti blanditiis ratione expedita
-          recusandae, facilis reiciendis odit? Quas illum pariatur accusantium
-          obcaecati nam itaque dicta, atque dolorem tempore placeat, totam fuga
-          sit neque fugit cum. Consequuntur eaque odit natus cupiditate magni.
-          Nemo possimus cumque iste animi suscipit officia, velit necessitatibus
-          amet iusto vitae veritatis, maiores doloribus voluptate deleniti eius
-          praesentium iure ipsam ipsa est quasi nulla! Provident aspernatur
-          libero voluptates impedit fugiat ullam excepturi eum pariatur facilis
-          praesentium alias, nostrum, sunt nisi rerum quo ipsum! Ipsa ullam vel
-          incidunt, quia sequi laborum architecto sed omnis. Culpa quas hic
-          nulla animi nam sunt vero, magni, modi similique repellat dignissimos
-          ex error ad labore quaerat placeat earum minus dolorum iusto, mollitia
-          neque sit deleniti laudantium! Dolor ut odit, iste laborum animi
-          molestiae iure neque nobis, a aperiam facere non voluptas fuga?
-          Voluptas veritatis qui vitae ratione repellendus facere illum
-        </p>
+          <p className="text-[26px] ml-[5vw] font-Poppins text-DarkText font-bold lg:text-[26px] md:ml-[-5vw] md:text-[24px] 2xl:text-[40px] 2xl:ml-[10vw] md:ml-[0vw]">
+            Get in touch
+          </p>
+          <p className={clsx(style.Text)}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Exercitationem facere voluptatum ab, cum fugiat asperiores amet sit
+            tenetur maiores unde! In omnis corporis, natus veniam aliquid
+            nesciunt quod pariatur inventore harum ratione velit facere nisi
+            possimus, repellat asperiores aliquam, officiis porro iste tempora
+            doloremque veritatis doloribus atque expedita! Maxime sunt obcaecati
+            debitis expedita. Aut debitis aliquam nostrum libero. Tempore quam
+            ipsum provident sequi saepe nobis repellat dolorem cumque similique
+            quibusdam quo, nam aliquam, quidem voluptate dignissimos vitae
+            dolor? Animi aspernatur, necessitatibus magni cumque minus quisquam
+            porro dolor sunt. Harum facilis sit quis, soluta ullam excepturi.
+            Unde eum debitis blanditiis molestias aperiam cupiditate labore,
+            soluta corporis repellat ab dolorem. Veritatis ad ducimus minima
+            quis iure, dolores optio aut assumenda fugiat enim velit quaerat
+            corrupti odit numquam commodi? Sequi velit facere aliquam aliquid.
+            Consectetur similique error nostrum fugit tenetur? Laborum incidunt
+            saepe amet optio consequuntur ipsa similique illum neque! Quas magni
+            id, dolorem possimus facilis autem fuga expedita natus qui porro
+            architecto tempora beatae laboriosam fugiat culpa animi distinctio
+            totam vitae sint dicta incidunt aut! Iusto ipsa ut recusandae
+            ducimus veniam deserunt omnis perferendis totam impedit natus
+            repudiandae voluptas illo aliquid repellendus, adipisci deleniti
+            reiciendis nobis a vero? Cupiditate aspernatur vero hic eaque nihil
+            id vitae dicta, molestias odit ducimus illum, fuga libero? Quae
+            pariatur cumque rerum quas doloremque id impedit modi minima
+            deleniti blanditiis ratione expedita recusandae, facilis reiciendis
+            odit? Quas illum pariatur accusantium obcaecati nam itaque dicta,
+            atque dolorem tempore placeat, totam fuga sit neque fugit cum.
+            Consequuntur eaque odit natus cupiditate magni. Nemo possimus cumque
+            iste animi suscipit officia, velit necessitatibus amet iusto vitae
+            veritatis, maiores doloribus voluptate deleniti eius praesentium
+            iure ipsam ipsa est quasi nulla! Provident aspernatur libero
+            voluptates impedit fugiat ullam excepturi eum pariatur facilis
+            praesentium alias, nostrum, sunt nisi rerum quo ipsum! Ipsa ullam
+            vel incidunt, quia sequi laborum architecto sed omnis. Culpa quas
+            hic nulla animi nam sunt vero, magni, modi similique repellat
+            dignissimos ex error ad labore quaerat placeat earum minus dolorum
+            iusto, mollitia neque sit deleniti laudantium! Dolor ut odit, iste
+            laborum animi molestiae iure neque nobis, a aperiam facere non
+            voluptas fuga? Voluptas veritatis qui vitae ratione repellendus
+            facere illum
+          </p>
         </div>
 
         <div className="ml-[-60vw] w-1/2 md:mt-[40vh] mt-[65vh] 2xl:mt-[5vh] 2xl:ml-[5vh] xl:mt-[5vh] lg:mt-[5vh] ">
-          <img src={Map} className="w-[100vw] xl:w-[30vw] 2xl:w-[35vw] 2xl:h-[30vw] ml-[-22vw] xl:ml-[25vw] h-[30vh] lg:ml-[20vw] md:ml-[-40vw] md:h-[40vw] 2xl:mt-[-10vh] xl:mt-[-5vh] 2xl:ml-[1vw] xl:h-[30vw]" alt="Map" />
+          <img
+            src={Map}
+            className="w-[100vw] xl:w-[30vw] 2xl:w-[35vw] 2xl:h-[30vw] ml-[-22vw] xl:ml-[25vw] h-[30vh] lg:ml-[20vw] md:ml-[-40vw] md:h-[40vw] 2xl:mt-[-10vh] xl:mt-[-5vh] 2xl:ml-[1vw] xl:h-[30vw]"
+            alt="Map"
+          />
           <div className="w-[29vw] grid grid-cols-1 2xl:grid-cols-2 ml-[-42vw] 2xl:ml-[1vw] md:mt-[-30vw] md:mb-[20vw] xl:ml-[0vh] mt-[3vh] lg:ml-[0vw] md:ml-[0vw]">
-            <div className={clsx(style.box)}> 
-            <p>Address :</p>
-            <input type="text" placeholder="JL.Cindelaras No.205A" name="" id="" />
+            <div className={clsx(style.box)}>
+              <p>Address :</p>
+              <input
+                type="text"
+                placeholder="JL.Cindelaras No.205A"
+                name=""
+                id=""
+              />
             </div>
-            <div className={clsx(style.box , "2xl:ml-[3vw]")}>
-            <p>Email :</p>
-            <input type="text" placeholder="info@travelaja.com" name="" id="" />
+            <div className={clsx(style.box, "2xl:ml-[3vw]")}>
+              <p>Email :</p>
+              <input
+                type="text"
+                placeholder="info@travelaja.com"
+                name=""
+                id=""
+              />
             </div>
             <div className={clsx(style.box)}>
-            <p>Phone :</p>
-            <input type="text" placeholder="+62 1234 432 567" name="" id="" />
+              <p>Phone :</p>
+              <input type="text" placeholder="+62 1234 432 567" name="" id="" />
             </div>
           </div>
         </div>
-
       </div>
       <div className="w-screen flex sm:flex-wrap overflow-x-auto mt-[5vh] 2xl:mt-[-10vh] xl:mt-[-10vh] lg:mt-[-10vh] md:mt-[0vh]">
         <Hutulbur />
--- front/src/pages/Post.jsx
+++ front/src/pages/Post.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import Arrow from "../img/Vector.png";
-import Lake from "../img/lake.png";
-import Mountain from "../img/mountain.png";
 import { Link } from "react-router-dom";
-import Map from "../img/map.png";
+import Arrow from "../img/Vector.png";
 
-export const Hutulbur = () => {
+export const Post = () => {
   return (
     <div>
-      <div
-        className="bg-cover w-[100vw] h-[35vh] p-[5vw] flex flex-col gap-[2.5vw]"
-        style={{ backgroundImage: `url(${Mountain})` }}
-      >
+      <div className="bg-cover w-[100vw] h-[35vh] p-[5vw] flex flex-col gap-[2.5vw]">
         <Link to="/">
           <img alt="" src={Arrow} />
         </Link>
@@ -24,53 +18,20 @@ export const Hutulbur = () => {
           <p className="font-bold text-[1.5vw] font-Poppins text-DarkText">
             Get in touch
           </p>
-          <div className="flex flex-col">
-            <p className="h-auto w-[38vw] font-[400] text-DarkText font-Poppins">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
-              urna, porttitor rhoncus dolor purus non enim praesent elementum
-              facilisis leo, vel fringilla est ullamcorper eget nulla.
-            </p>
-            <p className="h-auto w-[38vw] font-[400] font-Poppins text-DarkText">
-              enim lobortis scelerisque fermentum dui faucibus in ornare quam
-              viverra orci sagittis eu volutpat odio facilisis mauris sit amet
-              massa vitae tortor condimentum lacinia quis vel eros donec ac odio
-              tempor orci dapibus ultrices in iaculis nunc sed augue lacus,
-              viverra vitae congue eu, consequat ac felis donec et odio
-              pellentesque diam volutpat commodo sed egestas egestas fringilla
-              fau.
-            </p>
-            <p className="h-auto w-[38vw] font-[400] font-Poppins text-DarkText">
-              enim lobortis scelerisque fermentum dui faucibus in ornare quam
-              viverra orci sagittis eu volutpat odio facilisis mauris sit amet
-              massa vitae tortor condimentum lacinia quis vel eros donec ac odio
-              tempor orci dapibus ultrices in iaculis nunc sed augue lacus,
-              viverra vitae congue eu, consequat ac felis donec et odio
-              pellentesque diam volutpat commodo sed egestas egestas fringilla
-              fau.
-            </p>
-            <p className="h-auto w-[38vw] font-[400] font-Poppins text-DarkText">
-              enim lobortis scelerisque fermentum dui faucibus in ornare quam
-              viverra orci sagittis eu volutpat odio facilisis mauris sit amet
-              massa vitae tortor condimentum lacinia quis vel eros donec ac odio
-              tempor orci dapibus ultrices in iaculis nunc sed augue lacus,
-              viverra vitae congue eu.
-            </p>
-          </div>
+          <input className="text-[16px] w-[40vw] h-[35vh] border border-black rounded-xl pb-[28vh] pl-[27px] f-Poppins" />
 
-          <img alt="" src={Lake} className="rounded-2xl w-[38vw]" />
+          <img alt="" className="rounded-2xl w-[38vw]" />
           <div className="flex flex-col gap-3">
             <p className="font-normal text-[1.2vw] font-Poppins text-DarkText">
               Comment :
             </p>
-            <input className="w-[37vw] h-[15vh] border-2 border-black rounded-xl focus:border-sky-500" />
+            <input className="w-[40vw] h-[4vh] border border-black rounded-full focus:border-sky-500" />
             <button class="bg-Button hover:bg-ButtonHover font-[400] font-Poppins w-[5vw] h-[4vh] rounded-lg text-white">
               Send
             </button>
           </div>
         </div>
         <div className="flex flex-col w-[50%] h-auto gap-[5%]">
-          <img alt="" src={Map} className="w-[80%] h-[50%] rounded-lg" />
           <div className="flex flex-col h-auto w-[80%] gap-[7vh]">
             <div className="w-[100%] flex justify-between">
               <div className="w-auto flex justify-between w-[45%]">
@@ -135,4 +96,3 @@ export const Hutulbur = () => {
     </div>
   );
 };
-export default Hutulbur;
