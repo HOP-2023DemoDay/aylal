d71dfb4f9bbd7bab8505083c7b65ee52d9fc7320
Natsagdorj <natsagdorj@niggers.local>
2023-05-15 17:27:33 +0800

    post hutulbur

--- front/src/pages/Hutulbur.jsx
+++ front/src/pages/Hutulbur.jsx
@@ -6,6 +6,9 @@ import News from "../components/news";
 import clsx from "clsx";
 
 export const Hutulbur = () => {
+  const style = {
+    box: "flex justify-between w-[281px] mt-[2vh]",
+  };
   return (
     <div className="overflow-hidden w-screen  ">
       <div className="flex felx-col w-full h-52 bg-slate-500 w-full">
@@ -73,7 +76,7 @@ export const Hutulbur = () => {
           
           <div>
           <img src={Map} className="w-full h-[46vh] bg-slate-900 md:h-[34vh] md: w-[40vh]" alt="Map" />
-          {/* <div className="xl:w-[29vw] xl:h-[8vh]  lg:h-[16vh] md:h-[16vh] xl:mt-[3vh]">
+          <div className="xl:w-[29vw] xl:h-[8vh]  lg:h-[16vh] md:h-[16vh] xl:mt-[3vh]">
             <div className={clsx(style.box)}> 
             <p className="">Address :</p>
             <input type="text" placeholder="JL.Cindelaras No.205A" name="" id="" />
@@ -86,7 +89,7 @@ export const Hutulbur = () => {
             <p>Phone :</p>
             <input type="text" placeholder="+62 1234 432 567" name="" id="" />
             </div>
-            </div> */}
+            </div>
           </div>
 
 
